78448db00dbb0ca9f16c6aee13de72ae3aca788a
shafik720 <shafikrasel5@gmail.com>
2023-01-15 14:02:48 +0600

    working on header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,15 +9,16 @@ import homeLogo from '../../Utilities/img/home.png';
 
 const Header = () => {
     const moveHeader= () => {
-        document.querySelector('.header-div').classList.add('active');
+        // document.querySelector('.header-div').classList.add('active');
     }
     const moveHeader2 = () =>{
-        document.querySelector('.header-div').classList.remove('active');
+        // document.querySelector('.header-div').classList.remove('active');
     }
     return (
         <div onMouseEnter={moveHeader} onMouseLeave={moveHeader2} className="header-div">
             <div className="logo-icon">
                 <img src={homeLogo} alt=""/>
+                <p>Home</p>
             </div>
             <div className="logo-icon">
                 <img src={homeLogo} alt=""/>
