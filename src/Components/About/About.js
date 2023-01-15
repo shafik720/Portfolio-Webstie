6adbe1c61693cb6b9bad1c47c08a926356f23644
shafik720 <shafikrasel5@gmail.com>
2023-01-15 20:40:38 +0600

    working on banner

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,6 +8,9 @@ const About = () => {
                 <p>Some Info</p>
                 <h2>About Me</h2>
             </div>
+            <div className="about-desc">
+                <p>I'm a Full Stack Web Developer looking for a new role in an exciting company. I focus on writing accessible HTML, using modern CSS practice and writing clean Javascript. When writing javascript code, I mostly use React, but I can adapt to whatever tools are required. I'm based in Dhaka, Bangladesh, but I'm happy working remotely and have expreience in remote teams. When I'm note coding, you'll find me outdoors. I love being out in nature whether that's going for a walk, run or motorcycling. I'd love you to check out my work. </p>
+            </div>
         </div>
     );
 };
