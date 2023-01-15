2cfb7b9233135838e35f685c9e4965e13ca119ad
shafik720 <shafikrasel5@gmail.com>
2023-01-15 13:52:41 +0600

    working on header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+
+import homeLogo from '../../Utilities/img/home.png';
+
+
 
 const Header = () => {
     return (
-        <div>
-            <h2>Headers</h2>
+        <div className="header-div">
+            <div className="logo-icon">
+                <img src={homeLogo} alt=""/>
+            </div>
+            <div className="logo-icon">
+                <img src={homeLogo} alt=""/>
+            </div>
+            <div className="logo-icon">
+                <img src={homeLogo} alt=""/>
+            </div>
+            <div className="logo-icon">
+                <img src={homeLogo} alt=""/>
+            </div>
         </div>
     );
 };
