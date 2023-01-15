0d93cd609e10b005ca848ba7515f59181a310e0e
shafik720 <shafikrasel5@gmail.com>
2023-01-16 03:37:48 +0600

    tooltip added

--- src/Components/Projects/SingleProject/SingleProject.js
+++ src/Components/Projects/SingleProject/SingleProject.js
@@ -2,44 +2,62 @@ import React from 'react';
 import folderLogo from '../../../Utilities/img/logo/folder (1).png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { Button } from 'react-bootstrap';
 
 const SingleProject = (props) => {
-    const {id, github, liveSite, projectName, desc, technology} = props.index;
+    const { id, github, liveSite, projectName, desc, technology } = props.index;
+    const renderTooltip = props => (
+        <Tooltip {...props}>Github Repo Link</Tooltip>
+    );
+    const renderTooltip2 = props => (
+        <Tooltip {...props}>Live Site Link</Tooltip>
+    );
     return (
-            <div className="col-lg-6">
-                            <div className="h-100">
-                                <div className="single-projects h-100">
-                                    <div className="">
-                                        <div className="single-project-header">
-                                            <div className="project-header-left">
-                                                <img src={folderLogo} alt="" />
-                                            </div>
-                                            <div className="project-header-right">
-                                                <a href={github} target="_blank" rel="noopener noreferrer">
-                                                    <span><FontAwesomeIcon icon={faGithub} /></span>
-                                                </a>
-                                                <a href={liveSite} target="_blank" rel="noopener noreferrer">
-                                                    <span><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></span></a>
-                                            </div>
-                                        </div>
-                                        <div className="single-project-body">
-                                            <h2>{projectName}</h2>
-                                            <p>{desc}</p>
-                                        </div>
-                                    </div>
+        <div className="col-lg-6">
+            <div className="h-100">
+                <div className="single-projects h-100">
+                    <div className="">
+                        <div className="single-project-header">
+                            <div className="project-header-left">
+                                <img src={folderLogo} alt="" />
+                            </div>
+                            <div className="project-header-right">
+                                {/* github link for projects */}
 
-                                    <div className="single-project-footer">
-                                        <div className="project-technology">
-                                        {
-                                                technology.map(index=><span><p>{index}</p></span>)
-                                                }
-                                        </div>
-                                    </div>
-                                </div>
+                                <OverlayTrigger placement="left" overlay={renderTooltip}>
+                                    <a href={github} target="_blank" rel="noopener noreferrer">
+                                        <span><FontAwesomeIcon icon={faGithub} /></span>
+                                    </a>
+                                </OverlayTrigger>
+
+                                {/* live site link for project */}
+                                <OverlayTrigger placement="bottom" overlay={renderTooltip2}>
+                                    <a href={liveSite} target="_blank" rel="noopener noreferrer">
+                                        <span><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></span></a>
+                                </OverlayTrigger>
 
                             </div>
                         </div>
+                        <div className="single-project-body">
+                            <h2>{projectName}</h2>
+                            <p>{desc}</p>
+                        </div>
+                    </div>
+
+                    <div className="single-project-footer">
+                        <div className="project-technology">
+                            {
+                                technology.map(index => <span><p>{index}</p></span>)
+                            }
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 };
 
