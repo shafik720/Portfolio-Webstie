1b193c4d0ab59f90487c582de7c4789eb0e6ef04
shafik720 <shafikrasel5@gmail.com>
2023-01-15 18:41:04 +0600

    working on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
+      <div className="main-div">
+        <Banner></Banner>
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="banner-div">
+            <div className="banner-left-parent">
+                <div className="banner-left"></div>
+            </div>
+
+            <div className="banner-right">
+                <p>Hi <br/> I'm</p>
+                <h1>Shafiqul Hasan Russell</h1>
+                <p>A Front-End Web Developer passionate about creating interactive applications and experiences on the web.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
