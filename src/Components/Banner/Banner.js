a449e0c41d55c81c62e4a0f53b3604486c95ae06
shafik720 <shafikrasel5@gmail.com>
2023-01-15 19:58:20 +0600

    working on banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Banner.css';
+import twitterLogo from '../../Utilities/img/logo/twitter (3).svg';
+import githubLogo from '../../Utilities/img/logo/github.svg';
+import linkedLogo from '../../Utilities/img/logo/likedin (1).svg';
+
 
 const Banner = () => {
     return (
@@ -13,6 +17,23 @@ const Banner = () => {
                 <h2>I'm</h2>
                 <h1>Shafiqul Hasan Russell</h1>
                 <p>A Front-End Web Developer passionate about creating interactive applications and experiences on the web.</p>
+                <div className="social-icons">
+                    <div className="social-single-icon">
+                        <div className="first-logo">
+                            <img src={githubLogo} alt="" />
+                        </div>
+                    </div>
+                    <div className="social-single-icon">
+                        <div className="first-logo">
+                            <img src={linkedLogo} alt="" />
+                        </div>
+                    </div>
+                    <div className="social-single-icon">
+                        <div className="first-logo">
+                            <img src={twitterLogo} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
