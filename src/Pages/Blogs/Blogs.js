10249641068f35b1bf39007e6630ba587fa97192
Shafiqul Hasan Russell <shafiqulhasanrussell@Shafiquls-MacBook-Air.local>
2023-06-28 13:52:01 +0600

    before adding to db

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import JoditEditor from 'jodit-react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
+    const editor = useRef(null);
+    const [content, setContent] = useState('');
+
+    const handleSubmit = () =>{
+        console.log(content);
+    }
     return (
         <div>
-            <h2>Blog here</h2>
+            <div className="writting-div">
+                <JoditEditor
+                    ref={editor}
+                    value={content}
+                    tabIndex={1} // tabIndex of textarea
+                    onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                    onChange={newContent => { }}
+                />
+                <button onClick={handleSubmit} className='post-button'>Post Blog</button>
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+
         </div>
     );
 };
