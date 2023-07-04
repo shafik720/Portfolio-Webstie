08570bfe78f1e50780686afc4f74ef146fb62832
shafik720 <shafikrasel5@gmail.com>
2023-07-04 21:48:53 +0600

    adding login

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import './Login.css' ; 
+
+const Login = () => {
+    return (
+        <div className='login-div'>
+            <form action="">
+                <input type="text" name="" placeholder='Email' id="" />
+                <input type="password" name="" id="" placeholder='Password' />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
--- src/Utilities/Router/Router.js
+++ src/Utilities/Router/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../../Pages/Home/Home';
 import Blogs from '../../Pages/Blogs/Blogs';
+import Login from '../../Components/Login/Login';
 
 const Router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const Router = createBrowserRouter([
     {
         path : '/postBlogs',
         element : <Blogs></Blogs>
+    },
+    {
+        path : '/adminLogin',
+        element : <Login></Login>
     }
 ])
 export default Router;
