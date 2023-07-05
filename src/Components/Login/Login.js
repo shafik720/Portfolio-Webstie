8c70c4a4b96a1141ae3adff7f9cb50c761d052d1
shafik720 <shafikrasel5@gmail.com>
2023-07-05 21:10:19 +0600

    sdfs

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,12 +1,23 @@
-import React from 'react' ;
+import React, { useState } from 'react' ;
 import './Login.css' ; 
+import auth from '../../Utilities/firebase.init';
 
 const Login = () => {
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault() ; 
+        console.log(email, password) ;
+    }
+
+    console.log(auth.currentUser);
+    
     return (
         <div className='login-div'>
-            <form action="">
-                <input type="text" name="" placeholder='Email' id="" />
-                <input type="password" name="" id="" placeholder='Password' />
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" name="" placeholder='Email' id="" onBlur={e => setEmail(e.target.value)}/>
+                <input type="password" name="" id="" placeholder='Password' onBlur={e => setPassword(e.target.value)}/>
                 <button type="submit">Login</button>
             </form>
         </div>
