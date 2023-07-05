14c7330f26c6b7dcef7244b6f350f27641872146
shafik720 <shafikrasel5@gmail.com>
2023-07-06 02:39:01 +0600

    installed cfc auth

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,23 +1,40 @@
-import React, { useState } from 'react' ;
-import './Login.css' ; 
+import React, { useEffect, useState } from 'react';
+import './Login.css';
 import auth from '../../Utilities/firebase.init';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import {useAuthState} from 'react-firebase-hooks/auth'
 
 const Login = () => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, loading, error] = useAuthState(auth);
+
 
     const handleSubmit = (e) => {
-        e.preventDefault() ; 
-        console.log(email, password) ;
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(error);
+            });
     }
 
-    console.log(auth.currentUser);
-    
+    useEffect(()=>{
+        console.log(user);
+    },[user])
+
     return (
         <div className='login-div'>
             <form action="" onSubmit={handleSubmit}>
-                <input type="text" name="" placeholder='Email' id="" onBlur={e => setEmail(e.target.value)}/>
-                <input type="password" name="" id="" placeholder='Password' onBlur={e => setPassword(e.target.value)}/>
+                <input type="text" name="" placeholder='Email' id="" onBlur={e => setEmail(e.target.value)} />
+                <input type="password" name="" id="" placeholder='Password' onBlur={e => setPassword(e.target.value)} />
                 <button type="submit">Login</button>
             </form>
         </div>
