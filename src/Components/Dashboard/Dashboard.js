045eb661679b87c99e4b94bf0ff5e11f90214e73
shafik720 <shafikrasel5@gmail.com>
2023-07-06 21:31:16 +0600

    adding authorization

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './Dashboard.css';
+import auth from '../../Utilities/firebase.init';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Dashboard = () => {
-    return (
-        <div className='dashboard-div'>
+    const [user, loading, error] = useAuthState(auth);
+    console.log(user);
+
+    let content = null;
+    if (loading && !error) {
+        content = <p>Checking Authentication...</p>
+    }
+    if (!loading && error) {
+        content = <p>There was an error loading the page</p>
+    }
+    if (user?.email === 'shafikrasel5@gmail.com') {
+        content = <div className='dashboard-div'>
             <button>View Blogs</button>
             <button>Add Blogs</button>
             <button>Logout</button>
+        </div>
+    } else {
+        content = <p>You are not authorized to see this</p>
+
+    }
+    return (
+        <div className="">
+            {content}
         </div>
     );
 };
