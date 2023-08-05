027ee5750ae57c24e628b949bc54f934a187960b
shafik720 <shafikrasel5@gmail.com>
2023-08-05 23:00:53 +0600

    adding blog section

--- src/Pages/Blogs/DisplayBlogs/BlogCard/BlogCard.js
+++ src/Pages/Blogs/DisplayBlogs/BlogCard/BlogCard.js
@@ -2,35 +2,47 @@ import React from 'react';
 import './BlogCard.css';
 import { useContext } from 'react';
 import { ModalContext } from '../../../../Utilities/Context Api/ModalContext';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../../Utilities/firebase.init';
 
-const BlogCard = ({index}) => {
-    const {_id, content, category, title} = index;
+const BlogCard = ({ index }) => {
+    const { _id, content, category, title } = index;
+
+    // --- checking if the user is admin
+    const [user, loading, error] = useAuthState(auth);
+    console.log(user?.email);
 
     const tempContent = document.createElement('div');
-    tempContent.innerHTML = content ; 
+    tempContent.innerHTML = content;
     const textFromContent = tempContent.textContent;
 
-    const shortDesc = textFromContent.split("").slice(0,150).join("");
+    const shortDesc = textFromContent.split("").slice(0, 150).join("");
     // console.log(content);
 
     // --- using context to open popup display or modal 
-    const {openModal} = useContext(ModalContext) ; 
+    const { openModal } = useContext(ModalContext);
     const handleModal = () => {
-        openModal({desc : content, title});
+        openModal({ desc: content, title });
     }
-    return (
+    return ( 
+        loading ? <p>Loading....</p> :
         <div className='blog-card'>
             <div className="blog-title">
                 <h5>{title}</h5>
             </div>
-            <div className="blog-details">                
-            <p>{shortDesc}...</p>
+            <div className="blog-details">
+                <p>{shortDesc}  . . . . .
+                    <button onClick={handleModal}>Read More</button></p>
             </div>
             <div className="blog-footer">
                 <p>12 January 2023</p>
-                <button onClick={handleModal}>Read More</button>
+                {user?.email == 'shafikrasel5@gmail.com' && <div className="modify-button">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>}
             </div>
             <div className="blog-style"></div>
+
         </div>
     );
 };
