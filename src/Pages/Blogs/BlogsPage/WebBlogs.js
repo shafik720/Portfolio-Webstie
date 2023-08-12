09308ae743a7b0734dd127e2d6ee6cb7e84b595e
shafik720 <shafikrasel5@gmail.com>
2023-08-13 04:38:13 +0600

    making search functionality

--- src/Pages/Blogs/BlogsPage/WebBlogs.js
+++ src/Pages/Blogs/BlogsPage/WebBlogs.js
@@ -11,8 +11,24 @@ const WebBlogs = () => {
     const [data, setData] = useState([]);
     const [catname, setCatname] = useState('');
     const [search, setSearch] = useState('');
+    const [searchData, setSearchData] = useState([]);
 
-    console.log(search);
+    // --- searching 
+    useEffect(() => {
+        if (search !== '') {
+            const result = [];
+            data?.map(index => {
+                if (index.title.toLowerCase().includes(search.toLowerCase())) {
+                    // console.log('Got ya');
+                    result.push(index);
+                } else {
+                    // console.log('Not found');
+                }
+            })
+            setSearchData(result);
+        }
+    }, [search])
+
     useEffect(() => {
         // --- fetching data from server
         fetch(`https://server-for-my-portfolio.vercel.app/blogs/${params.category}`)
@@ -41,7 +57,6 @@ const WebBlogs = () => {
         else {
             setCatname('');
         }
-
     }, [params.category, params])
 
     // console.log(params.category);
@@ -50,12 +65,12 @@ const WebBlogs = () => {
             <h1><span className='category-special-word'>{catname} </span> related Blogs </h1>
             <hr />
             <div className="search-div">
-                <input type="text" name="" id="" onKeyUp={(e)=>setSearch(e.target.value)} />
+                <input type="text" name="" id="" onKeyUp={(e) => setSearch(e.target.value)} />
                 <button>Search</button>
             </div>
             <div className='blog-div'>
                 {/* <p>Total Blog Found : {data.length}</p> */}
-                {data?.map(index => <BlogCard index={index} key={index._id}></BlogCard>)}
+                {search === '' ? data?.map(index => <BlogCard index={index} key={index._id}></BlogCard>) : searchData?.map(index => <BlogCard index={index} key={index._id}></BlogCard>)}
                 {data?.length === 0 && <p>Loading...</p>}
             </div>
             {!data && <p className='not-found'>No Blogs Found ! <br />Please check back later :) </p>}
